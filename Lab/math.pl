585998e45028c06494244587e9c679cea5df69a8
Noah Solorzano <nsolorzano1@twu.edu>
2017-09-19 09:21:54 -0500

    Math program

---
+++ Lab/math.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use warnings;
+
+print "I need two numbers, give me the firt number: " ;
+$number1 = <STDIN>;
+print "Now the second:";
+$number2 = <STDIN>;
+print"$number1+$number2=";
+print$number1+$number2."\n";
+print"$number1-$number2=";
+print$number1-$number2. "\n";
+print"$number1*$number2=";
+print$number1*$number2."\n";
+print"$number1/$number2=";
+print$number1/$number2."\n";
+
+
