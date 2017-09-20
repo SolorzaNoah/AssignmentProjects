2ab6d6264ffc52a2b76fb6dd5d814efa6eee11eb
Noah Solorzano <nsolorzano1@twu.edu>
2017-09-20 14:20:47 -0500

    Madlib

---
+++ Homework/madlib.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+
+use warnings;
+
+print "Hi, you ready to make a story? Im gonna need some help, start by giving me a random word: ";
+
+$randomword=<STDIN>;
+chomp $randomword;
+
+print " Now give me a name:";
+
+$name1=<STDIN>;
+chomp $name1;
+
+print "Now give me 4 verbs:";
+
+$verb1=<STDIN>;
+chomp $verb1;
+$verb2=<STDIN>;
+chomp $verb2;
+$verb3=<STDIN>;
+chomp $verb3;
+$verb4=<STDIN>;
+chomp $verb4;
+
+print "Name a celebrity:";
+
+$celebrity=<STDIN>;
+chomp $celebrity;
+
+print "Name a song:";
+
+$songname=<STDIN>;
+chomp $songname;
+
+print "Almost done, name a body part:";
+
+$bodypart=<STDIN>;
+chomp $bodypart;
+
+print "Okay here goes";
+print "\n The other day,$name1 took me to the park to $verb4 and $verb3 , but I said no because I am vegan. I played my favorite song $songname to set the scene but my $bodypart was itchy so I decided to $verb2 with $celebrity who happened to be passing by. $name1 began to $verb1 so we went home and $randomword." ;
+
