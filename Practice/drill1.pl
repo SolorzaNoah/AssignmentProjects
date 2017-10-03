249d4f9b40c27871027b479d3052e1d076a11d4d
Noah Solorzano <nsolorzano1@twu.edu>
2017-10-03 10:26:03 -0500

    drillz

---
+++ Practice/drill1.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+use warnings;
+
+print "What\'s your favorite food\?\n"; 
+chomp($food = <STDIN>);
+
+print "I hate $food! It's the worst \n";
---
+++ Practice/drill2.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+use warnings;
+
+print "Give me a number that isn't exactly pleasing but if you had to choose a number between 1 and 10 you would\npick this because its simple and unsymmetrically pleasing:\n";
+$num=<STDIN>;
+
+if ($num==3) {
+
+	print "yay ':|'";
+	}
+	
