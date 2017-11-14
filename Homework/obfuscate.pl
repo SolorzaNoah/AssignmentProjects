e5508890f392e1084a5422f290038419d21eae12
Noah Solorzano <nsolorzano1@twu.edu>
2017-11-14 09:52:54 -0600

    re-upload ??

---
+++ Homework/obfuscate.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use warnings;
+
+open DATA, '<','data.csv' || die;
+
+@data = <DATA>;
+chomp(@data);
+close DATA;
+
+shift (@data);
+open DATA, '>', 'newdatafile.csv' || die;
+close DATA;
+
+foreach $individual (@data){
+		$individual=~ s/\r//;
+		@data2 = split(',',$individual);
+		open DATA, '>', 'newdatafile.csv' || die;
+		print DATA "$data2[3].$data2[5].$data2[6].$data2[7].$data2[8].$data2[9].\n";
+		close DATA;
+		print @data2;
+}
+		
