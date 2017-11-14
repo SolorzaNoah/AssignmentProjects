d4dce5b5b7ff7b566a673899610bbaa2c5355b5f
Noah Solorzano <nsolorzano1@twu.edu>
2017-11-14 16:49:14 +0000

    Update db_test.pl

--- Homework/db_test.pl
+++ Homework/db_test.pl
@@ -1,16 +1,6 @@
 #Jonathan Ninan,Rolando Negron,Noah Solorzano
 use DBI;
 
-<<<<<<< HEAD
-my $dbfile = "foo.db";
- 
-my $dsn      = "dbi:mysql:foo=$dbfile";
-my $user     = "student";
-my $password = "Welcome1!";
-my $dbh = DBI->connect($dsn, $user, $password, {
-});
- 
-=======
 my $driver = "mysql";
 my $dbfile = "foo";
 
@@ -39,4 +29,3 @@ while (@row = $sth->fetchrow_array()) {  # retrieve one row
 
 $sth->finish();
 $dbh->disconnect;
->>>>>>> 04604c92c2c35b52ec6938b8e841a8dee7f3c70b
