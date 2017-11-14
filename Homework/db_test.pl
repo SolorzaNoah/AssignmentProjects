121688efb2a09f163b157fdfb2cd3b6f7f9ff11c
Noah Solorzano <nsolorzano1@twu.edu>
2017-11-14 02:07:11 -0600

    database test

--- Homework/db_test.pl
+++ Homework/db_test.pl
@@ -1,18 +1,40 @@
 #!/usr/bin/perl
 
-use warnings;
+#Jonathan Ninan,Rolando Negron,Noah Solorzano
 use DBI;
 
-my $dbfile = "foo.db";
- 
-my $dsn      = "dbi:SQLite:foo=$dbfile";
-my $user     = "";
-my $password = "";
-my $dbh = DBI->connect($dsn, $user, $password, {
-   PrintError       => 0,
-   RaiseError       => 1,
-   AutoCommit       => 1,
-   FetchHashKeyName => 'NAME_lc',
-});
- 
+my $driver = "mysql";
+my $dbfile = "foo";
+
+my $dsn      = "DBI:$driver:database=$dbfile";
+my $user     = "student";
+my $password = "Welcome1!";
+my $dbh = DBI->connect($dsn, $user, $password) or die $DBI::errstr;
+
+#my $var = $dbh->quote("state");
+my $sql1 = ('SELECT state FROM data');
+my $sth = $dbh->prepare($sql1);
+$sth->execute();
+my @row;
+while (@row = $sth->fetchrow_array()) {  # retrieve one row
+    print join(", ", @row), "\n";
+}
+$sth->finish();
+
+my $sql2 = ('SELECT city FROM data');
+my $sth = $dbh->prepare($sql2);
+$sth->execute();
+my @row;
+while (@row = $sth->fetchrow_array()) {  # retrieve one row
+    print join(", ", @row), "\n";
+}
+$sth->finish();
+my $sql2 = ('SELECT city FROM data');
+my $sth = $dbh->prepare($sql2);
+$sth->execute();
+my @row;
+while (@row = $sth->fetchrow_array()) {  # retrieve one row
+    print join(", ", @row), "\n";
+}
+$sth->finish();
 $dbh->disconnect;
