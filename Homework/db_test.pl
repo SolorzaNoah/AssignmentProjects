afb2f74473bcc22ef7507d1480bba37f63164b39
Noah G. Solorzano <nsolorzano1@debx64template.twu.edu>
2017-11-14 09:59:41 -0600

    idk what this iss

--- Homework/db_test.pl
+++ Homework/db_test.pl
@@ -5,14 +5,9 @@ use DBI;
 
 my $dbfile = "foo.db";
  
-my $dsn      = "dbi:SQLite:foo=$dbfile";
-my $user     = "";
-my $password = "";
+my $dsn      = "dbi:mysql:foo=$dbfile";
+my $user     = "student";
+my $password = "Welcome1!";
 my $dbh = DBI->connect($dsn, $user, $password, {
-   PrintError       => 0,
-   RaiseError       => 1,
-   AutoCommit       => 1,
-   FetchHashKeyName => 'NAME_lc',
 });
  
-$dbh->disconnect;
